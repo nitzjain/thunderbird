aee983185ef9ffa2d82c00afaa3b19fb8a1ffef8
Dheeraj Dake <dheeraj.dake@sjsu.edu>
2015-11-03 04:33:00 -0800

    Final motor

--- L5_Application/periodic_scheduler/period_callbacks.cpp
+++ L5_Application/periodic_scheduler/period_callbacks.cpp
@@ -44,6 +44,60 @@ const uint32_t PERIOD_TASKS_STACK_SIZE_BYTES = (512 * 4);
 //can message id
 can_msg_t control;
 
+unsigned int valueNeeded;
+
+#define up    7.9
+#define up_L  7.6
+#define up_R  7.6
+#define down  5.5
+#define SL    5
+#define SR    8
+
+#if 0
+class MotorController1: public SingletonTemplate<MotorController1>
+{
+    public:
+
+    void setDriveMotor(float v)
+    {
+        mDriveMotor.set(v);
+    }
+    private:
+    PWM mDriveMotor;
+
+    MotorController1() :
+    mDriveMotor(PWM::pwm1, 50)
+    {
+
+    }
+    friend class SingletonTemplate<MotorController1>;
+    ///< Friend class used for Singleton Tem
+
+};
+
+class MotorController2: public SingletonTemplate<MotorController2>
+{
+    public:
+    void setSteerMotor(float v)
+    {
+        mSteerMotor.set(v);
+    }
+
+    private:
+    PWM mSteerMotor;
+
+    MotorController2() :
+    mSteerMotor(PWM::pwm2, 50)
+    {
+
+    }
+    friend class SingletonTemplate<MotorController2>;
+    ///< Friend class used for Singleton Tem
+
+};
+
+#endif
+
 class MotorController: public SingletonTemplate<MotorController>
 {
     public:
@@ -52,28 +106,47 @@ class MotorController: public SingletonTemplate<MotorController>
         {
             mDriveMotor.set(v);
         }
-        void setSteerMotor(float v)
-        {
-            mSteerMotor.set(v);
-        }
-
     private:
         PWM mDriveMotor;
-        PWM mSteerMotor;
+       // PWM mSteerMotor;
 
-        MotorController() :
-                mDriveMotor(PWM::pwm1, 50), mSteerMotor(PWM::pwm2, 50)
+        MotorController() : mDriveMotor(PWM::pwm1)//, mSteerMotor(PWM::pwm2)
         {
 
         }
+
         friend class SingletonTemplate<MotorController> ;
-        ///< Friend class used for Singleton Tem
+        ///< Friend class used for Singleton Template
+};
+
+//MotorController &M = MotorController::getInstance();
+
+class MotorController2: public SingletonTemplate<MotorController2>
+{
+    public:
+    void setSteerMotor(float v)
+    {
+        mSteerMotor.set(v);
+    }
+
+    private:
+    PWM mSteerMotor;
+
+    MotorController2() : mSteerMotor(PWM::pwm2)
+    {
+
+    }
+    friend class SingletonTemplate<MotorController2>;
+    ///< Friend class used for Singleton Tem
 
 };
 
 void period_1Hz(void)
 {
-    //should test in 1Hz
+    //LE.off(1);
+    //LE.off(2);
+    //LE.off(3);
+    //LE.off(4);
 }
 
 void period_10Hz(void)
@@ -83,46 +156,54 @@ void period_10Hz(void)
 
 void period_100Hz(void)
 {
-    static int flag = 0;
     MotorController &M = MotorController::getInstance();
+    MotorController2 &M2 = MotorController2::getInstance();
+    //should test in 1Hz
+    static int flag = 0;
 
     if (flag == 0)
     {
         M.setDriveMotor(7.5); //Initialize the motor
+        M2.setSteerMotor(7);
+        printf("Initialzed");
+        // M.setDriveMotor(up); //Move forward
         flag++;
     }
 
     /* Move forward - */
-    if (CAN_rx(can1, &control, 10))
+    if (CAN_rx(can1, &control, 1))
     {
         if (control.msg_id == forward) //go forward - 000
         {
-            M.setDriveMotor(8); //Move forward
-            M.setSteerMotor(7);
+            M.setDriveMotor(up); //Move forward
+            M2.setSteerMotor(7);
+             LE.toggle(1);
         }
         else if (control.msg_id == left) //go left 001, 011
         {
-            M.setDriveMotor(8);
-            M.setSteerMotor(6);
+            M2.setSteerMotor(SL);
+            M.setDriveMotor(up_L);
+             LE.on(2);
         }
         else if (control.msg_id == right) //go right 010, 100, 110
         {
-            M.setDriveMotor(8);
-            M.setSteerMotor(8.5);
+            M2.setSteerMotor(SR);
+            M.setDriveMotor(up_R);
+            LE.on(3);
         }
         else if (control.msg_id == reverse) //go back 111, 101
         {
-            //M.setDriveMotor(7.5);
-            M.setSteerMotor(8.5);
-            M.setDriveMotor(6);
+            M.setDriveMotor(7.5);
+            M.setDriveMotor(down);
+            M2.setSteerMotor(SR);
+            LE.toggle(4);
         }
         else if (control.msg_id == stop)
         {
             M.setDriveMotor(7.5);
-            M.setSteerMotor(7);
+            M2.setSteerMotor(7);
         }
     }
-
 }
 
 void period_1000Hz(void)

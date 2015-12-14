4c3d8e1741ae6b0091f20125e1ac2ac5fd499afa
vishnumandalapu <vishnu.mandalapu@gmail.com>
2015-12-13 22:00:47 -0800

    resolving conflict

--- L5_Application/motor_directions/motor_directions.cpp
+++ L5_Application/motor_directions/motor_directions.cpp
@@ -13,6 +13,7 @@ extern uint8_t speed_counter;
 /**
  * Gives the direction based on sensor values.
  */
+<<<<<<< HEAD
 
 direction_t direction_computation(int sensor_left, int sensor_straight, int sensor_right, int sensor_reverse)
 {
@@ -78,8 +79,73 @@ direction_t direction_computation(int sensor_left, int sensor_straight, int sens
                  return reverse;
          else
                  return stop;
+=======
+bool isnearobstacle(int sensor_left,int sensor_straight,int sensor_right)
+{
+    if(sensor_left<SENSOR_THRESHOLD||sensor_straight<SENSOR_THRESHOLD||sensor_right<SENSOR_THRESHOLD)
+     return true;
+}
+direction_t fardirection_computation(int sensor_left,int sensor_straight,int sensor_right,int sensor_reverse,direction_t neardirection)
+{
+    if(sensor_left<FAR_SENSOR_THRESHOLD&&sensor_right<FAR_SENSOR_THRESHOLD)
+        return straight;
+    else if(sensor_left<FAR_SENSOR_THRESHOLD&&sensor_straight<FAR_SENSOR_THRESHOLD)
+        return slight_right;
+    else if(sensor_left<FAR_SENSOR_THRESHOLD&&sensor_right<FAR_SENSOR_THRESHOLD)
+        return slight_left;
 
+    return neardirection;
+>>>>>>> dc69a119307639852584e50fd2b5638aa2e32a21
+
+}
+direction_t direction_computation(int sensor_left, int sensor_straight, int sensor_right, int sensor_reverse)
+{
+    if (sensor_left >= SENSOR_THRESHOLD && sensor_straight >= SENSOR_THRESHOLD
+            && sensor_right >= SENSOR_THRESHOLD)
+    {
+        return straight; // it should follow GPS co ordinates
     }
-#endif
+    else if (sensor_left >= SENSOR_THRESHOLD
+            && sensor_straight >= SENSOR_THRESHOLD
+            && sensor_right < SENSOR_THRESHOLD)
+    {
+        return straight;
+    }
+    else if (sensor_left >= SENSOR_THRESHOLD
+            && sensor_straight < SENSOR_THRESHOLD
+            && sensor_right >= SENSOR_THRESHOLD)
+    {
+        return right;
+    }
+    else if (sensor_left >= SENSOR_THRESHOLD
+            && sensor_straight < SENSOR_THRESHOLD
+            && sensor_right < SENSOR_THRESHOLD)
+    {
+        return left;
+    }
+    else if (sensor_left < SENSOR_THRESHOLD
+            && sensor_straight >= SENSOR_THRESHOLD
+            && sensor_right >= SENSOR_THRESHOLD)
+    {
+        return straight;
+    }
+    else if (sensor_left < SENSOR_THRESHOLD
+            && sensor_straight >= SENSOR_THRESHOLD
+            && sensor_right < SENSOR_THRESHOLD)
+    {
+        return straight;
+    }
+    else if (sensor_left < SENSOR_THRESHOLD
+            && sensor_straight < SENSOR_THRESHOLD
+            && sensor_right >= SENSOR_THRESHOLD)
+    {
+        return right;
+    }
+    else if (sensor_left < SENSOR_THRESHOLD
+            && sensor_straight < SENSOR_THRESHOLD
+            && sensor_right < SENSOR_THRESHOLD)
+    {
+        return stop;
+    }
 }
 
